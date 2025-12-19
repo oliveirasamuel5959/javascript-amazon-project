f96d06ea045343ef4aefd15790500286c610fc07
Samuel-Work <patriciosamuel097@gmail.com>
2025-12-19 15:58:47 -0300

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,17 +1,25 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
-  },
-  {
-    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    quantity: 5
-  },
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1
+    },
+    {
+      productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+      quantity: 5
+    },
+  ];
+}
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
 
@@ -31,6 +39,9 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+
+  saveToStorage();
+
 };
 
 export function removeFromCart(productId) {
@@ -43,5 +54,7 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 
 };
