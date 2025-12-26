34d3e547ed3a0f984759ad5a62b416f0a6f16b27
Samuel-Work <patriciosamuel097@gmail.com>
2025-12-26 15:31:55 -0300

    Add Async Await test code to checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,10 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
+function loadPage() {
+  console.log('Page loaded');
+}
+
 new Promise((resolve) => {
   
 })
